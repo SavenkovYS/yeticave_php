bf062d77c7c447619831e3d21c942cc5ca51b92a
Юрий Савенков <yura-savenkov1@yandex.ru>
2021-02-06 14:31:31 +0300

    lot query

--- templates/index.php
+++ templates/index.php
@@ -34,7 +34,9 @@
                 </div>
                 <div class="lot__info">
                     <span class="lot__category"><?=$value['category']; ?></span>
-                    <h3 class="lot__title"><a class="text-link" href="lot.html"><?=$value['name']; ?></a></h3>
+                    <h3 class="lot__title">
+                        <a class="text-link" href="<?="lot.php?id=" . $value['id']; ?>"><?=$value['name']; ?></a>
+                    </h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
