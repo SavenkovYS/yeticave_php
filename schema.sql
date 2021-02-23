ba439b0207cb360d4b3c1e2018cfb027955ffaf0
Юрий Савенков <yura-savenkov1@yandex.ru>
2021-02-23 18:07:11 +0300

    schema

--- schema.sql
+++ schema.sql
@@ -50,3 +50,92 @@ CREATE TABLE users (
 );
 CREATE UNIQUE INDEX id ON users(id);
 CREATE UNIQUE INDEX email ON users(email);
+
+
+-- -----------------------------------------------------
+-- Schema yeticave
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `yeticave` DEFAULT CHARACTER SET utf8 ;
+USE `yeticave` ;
+
+-- -----------------------------------------------------
+-- Table `yeticave`.`users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `yeticave`.`users` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(60) NOT NULL,
+    `password` VARCHAR(64) NOT NULL,
+    `name` VARCHAR(64) NOT NULL,
+    `avatar` VARCHAR(128) NULL,
+    `message` TEXT NOT NULL,
+    `reg_ts` INT NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE)
+    ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `yeticave`.`categories`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `yeticave`.`categories` (
+   `id` INT NOT NULL AUTO_INCREMENT,
+   `name` VARCHAR(60) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE)
+    ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `yeticave`.`lots`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `yeticave`.`lots` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `price` INT NOT NULL,
+    `step` INT NOT NULL,
+    `img` VARCHAR(124) NOT NULL,
+    `description` TEXT NOT NULL,
+    `create_ts` INT NULL,
+    `expire_ts` INT NULL,
+    `winner_id` INT NULL,
+    `users_id` INT NOT NULL,
+    `categories_id` INT NOT NULL,
+    PRIMARY KEY (`id`),
+    INDEX `fk_lots_users_idx` (`users_id` ASC) VISIBLE,
+    INDEX `fk_lots_categories1_idx` (`categories_id` ASC) VISIBLE,
+    CONSTRAINT `fk_lots_users`
+    FOREIGN KEY (`users_id`)
+    REFERENCES `yeticave`.`users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+    CONSTRAINT `fk_lots_categories1`
+    FOREIGN KEY (`categories_id`)
+    REFERENCES `yeticave`.`categories` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+    ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `yeticave`.`bets`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `yeticave`.`bets` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `value` INT NOT NULL,
+    `create_ts` INT NOT NULL,
+    `users_id` INT NOT NULL,
+    `lots_id` INT NOT NULL,
+    PRIMARY KEY (`id`),
+    INDEX `fk_bets_users1_idx` (`users_id` ASC) VISIBLE,
+    INDEX `fk_bets_lots1_idx` (`lots_id` ASC) VISIBLE,
+    CONSTRAINT `fk_bets_users1`
+    FOREIGN KEY (`users_id`)
+    REFERENCES `yeticave`.`users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+    CONSTRAINT `fk_bets_lots1`
+    FOREIGN KEY (`lots_id`)
+    REFERENCES `yeticave`.`lots` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+    ENGINE = InnoDB;
